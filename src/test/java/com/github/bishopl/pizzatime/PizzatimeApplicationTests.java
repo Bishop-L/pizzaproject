f094e2572e4cd01eb11a2971a469d3a85f20463f
Bishop <brittany.lawson@outlook.com>
2023-11-11 13:39:31 -0800

    First Commit, Boilerplate code

---
+++ src/test/java/com/github/bishopl/pizzatime/PizzatimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bishopl.pizzatime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzatimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
