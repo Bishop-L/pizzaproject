66aad2ca35a5380e859a4e5099f0d7c99cd0b4c5
Bishop <brittany.lawson@outlook.com>
2023-11-11 19:01:31 -0800

    PizzaPrices in memory storage

---
+++ src/main/java/com/github/bishopl/pizzatime/config/PizzaPrices.java
@@ -0,0 +1,29 @@
+package com.github.bishopl.pizzatime.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class represents the base prices for pizzas of different sizes.
+ */
+import com.github.bishopl.pizzatime.model.PizzaSize;
+
+public class PizzaPrices {
+    private static final Map<PizzaSize, Double> BASE_PRICE = new HashMap<>();
+    
+    static {
+        BASE_PRICE.put(PizzaSize.SMALL, 4.0);
+        BASE_PRICE.put(PizzaSize.MEDIUM, 7.0);
+        BASE_PRICE.put(PizzaSize.LARGE, 9.0);
+    }
+
+    /**
+     * Returns the base price for a pizza of a given size.
+     * @param pizzaSize the size of the pizza
+     * @return the base price of the pizza
+     */
+    public static double getBasePrice(PizzaSize pizzaSize) {
+        return BASE_PRICE.get(pizzaSize);
+    }
+
+}
---
+++ src/test/java/com/github/bishopl/pizzatime/PizzaPricesTest.java
@@ -0,0 +1,18 @@
+package com.github.bishopl.pizzatime;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.github.bishopl.pizzatime.model.PizzaSize;
+import com.github.bishopl.pizzatime.config.PizzaPrices;
+
+public class PizzaPricesTest {
+    
+    @Test
+    public void testGetBasePrice() {
+        assertEquals(4.0, PizzaPrices.getBasePrice(PizzaSize.SMALL));
+        assertEquals(7.0, PizzaPrices.getBasePrice(PizzaSize.MEDIUM));
+        assertEquals(9.0, PizzaPrices.getBasePrice(PizzaSize.LARGE));
+    }
+    
+}
