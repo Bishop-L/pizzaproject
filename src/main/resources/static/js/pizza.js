1c78319581a0ceb1260886d514a743cc6e43bc83
Bishop <brittany.lawson@outlook.com>
2023-11-14 23:38:14 -0800

    A working UI

--- src/main/resources/static/js/pizza.js
+++ src/main/resources/static/js/pizza.js
@@ -5,34 +5,117 @@ app.controller('PizzaOrderController', function ($http) {
     var $ctrl = this;
     $ctrl.apiUrl = '/v1/app/orders';
 
-    $ctrl.startPizzaOrder = function (toppings) {
-        return $http.post(`${$ctrl.apiUrl}`, toppings);
+    $ctrl.startPizzaOrder = function (pizzas) {
+        return $http.post(`${$ctrl.apiUrl}`, pizzas);
     };
 
+     // Available toppings
+     $ctrl.availableToppings = [
+        { type: 'Cheese', amount: 'NONE'}, 
+        { type: 'Pepperoni', amount: 'NONE' }, 
+        { type: 'Sausage', amount: 'NONE' },  
+        { type: 'Bacon', amount: 'NONE' },
+        { type: 'Ham', amount: 'NONE' },
+        { type: 'Mushrooms', amount: 'NONE' },
+        { type: 'Olives', amount: 'NONE' },
+        { type: 'Onions', amount: 'NONE' },
+        { type: 'Peppers', amount: 'NONE' },
+        { type: 'Pineapples', amount: 'NONE' }
+    ];
+
+    // Pizza sizes
+    $ctrl.pizzaSizes = ['SMALL', 'MEDIUM', 'LARGE'];  
+
+    // Start a new pizza order on initialization
     $ctrl.$onInit = function () {
-        // Start a new pizza order on initialization
         var pizzas = [{}]; //Add the default pizzas
         $ctrl.startPizzaOrder(pizzas).then(function (response) {
-            $ctrl.pizzaOrder = response.data;
-            console.log(response.data);
+            $ctrl.updatePizzaOrder(response);
         });
     };
 
-    $ctrl.pizzaSizes = ['SMALL', 'MEDIUM', 'LARGE'];
+    // Change pizza size
+    $ctrl.changePizzaSize = function (pizzaSize) {
+        var orderId = $ctrl.pizzaOrder.id;
+        return $http.patch(`${$ctrl.apiUrl}/${orderId}/pizzas/0`, pizzaSize);
+    };
 
-    // Available toppings
-    $ctrl.availableToppings = [
-        { name: 'Cheese', amount: 2 }, 
-        { name: 'Pepperoni', amount: 0 }, 
-        { name: 'Sausage', amount: 0 },  
-        { name: 'Bacon', amount: 0 },
-        { name: 'Ham', amount: 0 },
-        { name: 'Mushrooms', amount: 0 },
-        { name: 'Olives', amount: 0 },
-        { name: 'Onions', amount: 0 },
-        { name: 'Peppers', amount: 0 },
-        { name: 'Pineapples', amount: 0 }
-    ];
+    // Update or add a topping
+    $ctrl.updatePizzaTopping = function (topping) {
+        var orderId = $ctrl.pizzaOrder.id;
+        return $http.post(`${$ctrl.apiUrl}/${orderId}/pizzas/0/toppings`, topping);
+    };
+
+    // Delete a topping ($http.delete does not support a body)
+    $ctrl.deletePizzaTopping = function (topping) {
+        var orderId = $ctrl.pizzaOrder.id;
+        var config = {
+            method: 'DELETE',
+            url: `${$ctrl.apiUrl}/${orderId}/pizzas/0/toppings`,
+            data: topping,
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            }
+        };
+    
+        return $http(config);
+    };
+
+    // Update available toppings based on the pizza order
+    $ctrl.updateAvailableToppings = function (pizzaOrder) {
+        angular.forEach(pizzaOrder.pizzas[0].toppings, function (responseTopping) {
+            var matchingTopping = $ctrl.availableToppings.find(function (availableTopping) {
+                return availableTopping.type.toUpperCase() === responseTopping.type;
+            });
+
+            if (matchingTopping) {
+                matchingTopping.amount = responseTopping.amount;
+                matchingTopping.isToppingSelected = responseTopping.amount !== 'NONE';
+            }
+        });
+    };
+
+    // Call right api call depending on the topping amount
+    $ctrl.toppingChanged = function (topping) {
+        toppingData = {};
+        toppingData['type'] = topping.type.toUpperCase();
+        
+        if(!topping.isToppingSelected){
+            // Delete topping
+            topping.amount = 'REGULAR';
+            toppingData['amount'] = topping.amount;
+            $ctrl.deletePizzaTopping(toppingData).then(function (response) {
+                $ctrl.updatePizzaOrder(response);
+            });
+            topping.amount = 'NONE';
+        } else {
+            // Update topping
+            if(topping.amount === 'NONE'){
+                topping.amount = 'REGULAR';
+            }
+            toppingData['amount'] = topping.amount;
+            $ctrl.updatePizzaTopping(toppingData).then(function (response) {
+                $ctrl.updatePizzaOrder(response);
+            });
+        }
+    };
+
+    // Update pizza size
+    $ctrl.sizeChanged = function () {
+        var size = $ctrl.pizzaOrder.pizzas[0].size.toUpperCase();
+
+        $ctrl.changePizzaSize(size).then(function (response) {
+            $ctrl.updatePizzaOrder(response);
+        });
+
+    }
+
+    // Update the current pizza order
+    $ctrl.updatePizzaOrder = function (response) {
+        $ctrl.pizzaOrder = response.data;
+        $ctrl.updateAvailableToppings(response.data);
+    };  
+
 });
 
 // Pizza Order Component
@@ -42,13 +125,5 @@ app.component('pizzaOrder', {
     controllerAs: '$ctrl',
     $onInit: function () {
         var $ctrl = this;    
-
-        // Start a new pizza order
-        var pizzas = []; //Add empty array of pizzas to begin
-        $ctrl.startPizzaOrder(pizzas).then(function (response) {
-            $ctrl.newPizzaOrder = response.data;
-        });
-    },
-
-    // Add other methods for updating, deleting, etc.
+    },     
 });
